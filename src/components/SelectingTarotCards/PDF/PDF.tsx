1be394f255d7e9c26cd4a390925ebf7ff5c7f346
rohit-maheshwari <rohit.mstar@gmail.com>
2024-04-09 18:39:37 -0700

    pdf generation phase 1

--- src/components/SelectingTarotCards/PDF/PDF.tsx
+++ src/components/SelectingTarotCards/PDF/PDF.tsx
@@ -1,34 +1,40 @@
 import { Component } from 'react';
-import { Page, Circle, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, Image, StyleSheet } from '@react-pdf/renderer';
 import { tarotcards } from "../tarotcards";
 
+const image = require('../../../images/FrontTarotCards/Scandal.png')
+
 const styles = StyleSheet.create({
-    page: { padding: 60 },
-    title: { marginTop: "90%" },
-    emphasis: { fontFamily: 'Helvetica-Bold', color: '#F22300' },
-    div: { display: "flex", flexDirection: 'row', gap: 10, marginTop: 10,  },
-    breakable1: { width: '33%', height: 100, backgroundColor: 'green' },
-    breakable2: { width: '33%', height: 100, backgroundColor: 'red' },
-    breakable3: { width: '33%', height: 300, backgroundColor: 'blue' },
-    circleWrap: {
-      bottom: 30,
-      left: 0,
-      right: 0,
-      display: "flex", 
-      flexDirection: 'row', 
-      justifyContent: 'center', 
-      alignItems: 'center',
+    page: {
+      padding: 60,
+      flexDirection: 'column',
     },
-    circle: {
-        width: 20,
-        height: 20,
-        borderRadius: '50%',
-        backgroundColor: '#ccc',
-        flexDirection: 'row',
-        margin: 25,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
+    card: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    imageContainer: {
+      width: '30%',
+      marginRight: 10,
+    },
+    image: {
+      width: '100%',
+      height: 'auto',
+    },
+    textContainer: {
+      flexDirection: 'column',
+      width: '80%',
+      justifyContent: 'center',
+    },
+    question: {
+      fontSize: 12,
+      marginBottom: 5,
+    },
+    response: {
+      fontSize: 12,
+      marginTop: 5,
+    },
 });
 
 type TarotCardType = {
@@ -38,58 +44,14 @@ type TarotCardType = {
     color: string
 }
 
-type ProgressBarPDFState = {
-
-}
-
-type ProgressBarPDFProps = {
-    allCards: typeof tarotcards
-    finishedCards: {[key: string]: boolean}
-}
-
-class ProgressBarPDF extends Component<ProgressBarPDFProps, ProgressBarPDFState> {
-    constructor(props: ProgressBarPDFProps) {
-        super(props);
-    }
-
-    handleDrawCircles = () : JSX.Element => {
-        const circles: JSX.Element[] = [];
-        console.log(this.props.finishedCards)
-        for (let i: number = 0; i < this.props.allCards.length; i++) {
-            const name = this.props.allCards[i].title;
-            if (this.props.finishedCards[name]) {
-                circles.push(<View style={{...styles.circle, backgroundColor: this.props.allCards[i].color}}/>);
-            } else {
-                circles.push(<View style={styles.circle}/>);
-            }
-        }
-
-        
-        return (
-            <View>
-                {circles.map((aCircle: JSX.Element, index: number) => {
-                    console.log(aCircle);
-                    return (aCircle);
-                })}
-            </View>
-        )
-    }
-
-    
-    render = (): JSX.Element => {
-        return (
-            this.handleDrawCircles()
-        )
-    };
-}
-
 type DocState = {
-
+    finished: TarotCardType[]
 }
 
 type DocProps = {
     allCards: TarotCardType[],
-    finishedCards: {[key: string]: boolean}
+    finishedCards: {[key: string]: boolean},
+    responses: {[key: string]: string}
 }
 
 class Doc extends Component<DocProps, DocState> {
@@ -97,29 +59,44 @@ class Doc extends Component<DocProps, DocState> {
     constructor(props: DocProps) {
         super(props);
     
-        this.state = {}
+        this.state = { finished: [] }
     }
 
     render = (): JSX.Element  => {
+        let f = []
+        for (let key of Object.keys(this.props.finishedCards).filter(key => this.props.finishedCards[key])) {
+            console.log(key)
+            for (let card of tarotcards) {
+                if (key === card.title) {
+                    f.push(card);
+                    break;
+                }
+            }
+        }
+        const orderMap = new Map(tarotcards.map((item, index) => [item.title, index]));
+
+        f.sort((a, b) => {
+            return (orderMap.get(a.title) || 0) - (orderMap.get(b.title) || 0);
+          });
+
         return (
             <Document>
                 <Page style={styles.page} size="A4" wrap>
-                    {/* <View style={styles.div}>
-                        <View style={styles.breakable1} wrap={false} />
-                        <View style={styles.breakable2} wrap={false} />
-                        <View style={styles.breakable3} wrap={false} />
+                    <View>
+                        {f.map((card: TarotCardType, index: number) => {
+                            return (
+                                <View key={index} style={styles.card}>
+                                    <View style={styles.imageContainer}>
+                                        <Image src={card.image} style={styles.image}/>
+                                    </View>
+                                    <View style={styles.textContainer} wrap={false}>
+                                        <Text style={styles.question}>{card.questions[0]}</Text>
+                                        <Text style={styles.response}>{this.props.responses[card.title]}</Text>
+                                    </View>
+                                </View>
+                            )
+                        })}
                     </View>
-                    <View style={styles.div}>
-                        <View style={styles.breakable1} wrap={false} />
-                        <View style={styles.breakable2} wrap={false} />
-                        <View style={styles.breakable3} wrap={false} />
-                    </View>
-                    <View style={styles.div}>
-                        <View style={styles.breakable1} wrap={false} />
-                        <View style={styles.breakable2} wrap={false} />
-                        <View style={styles.breakable3} wrap={false} />
-                    </View> */}
-                    <ProgressBarPDF allCards={this.props.allCards} finishedCards={this.props.finishedCards}/>
                 </Page>
             </Document>
 
