4e5c6a6d54bf7b966bf33245a35d05096e93c3b2
Julie Yu <julie.12.yu@gmail.com>
2024-04-09 17:15:26 -0700

    badoink remove progresbar :c

--- src/components/SelectingTarotCards/PDF/PDF.tsx
+++ src/components/SelectingTarotCards/PDF/PDF.tsx
@@ -5,19 +5,9 @@ import { tarotcards } from "../tarotcards";
 const styles = StyleSheet.create({
     page: { padding: 60 },
     title: { marginTop: "90%" },
-    emphasis: { fontFamily: 'Helvetica-Bold', color: '#F22300' },
     div: { display: "flex", flexDirection: 'row', gap: 10, marginTop: 10,  },
-    breakable1: { width: '33%', height: 100, backgroundColor: 'green' },
-    breakable2: { width: '33%', height: 100, backgroundColor: 'red' },
-    breakable3: { width: '33%', height: 300, backgroundColor: 'blue' },
     circleWrap: {
-      bottom: 30,
-      left: 0,
-      right: 0,
-      display: "flex", 
-      flexDirection: 'row', 
-      justifyContent: 'center', 
-      alignItems: 'center',
+        flexDirection: 'row'
     },
     circle: {
         width: 20,
@@ -36,51 +26,6 @@ type TarotCardType = {
     image: string,
     questions: string[],
     color: string
-}
-
-type ProgressBarPDFState = {
-
-}
-
-type ProgressBarPDFProps = {
-    allCards: typeof tarotcards
-    finishedCards: {[key: string]: boolean}
-}
-
-class ProgressBarPDF extends Component<ProgressBarPDFProps, ProgressBarPDFState> {
-    constructor(props: ProgressBarPDFProps) {
-        super(props);
-    }
-
-    handleDrawCircles = () : JSX.Element => {
-        const circles: JSX.Element[] = [];
-        console.log(this.props.finishedCards)
-        for (let i: number = 0; i < this.props.allCards.length; i++) {
-            const name = this.props.allCards[i].title;
-            if (this.props.finishedCards[name]) {
-                circles.push(<View style={{...styles.circle, backgroundColor: this.props.allCards[i].color}}/>);
-            } else {
-                circles.push(<View style={styles.circle}/>);
-            }
-        }
-
-        
-        return (
-            <View>
-                {circles.map((aCircle: JSX.Element, index: number) => {
-                    console.log(aCircle);
-                    return (aCircle);
-                })}
-            </View>
-        )
-    }
-
-    
-    render = (): JSX.Element => {
-        return (
-            this.handleDrawCircles()
-        )
-    };
 }
 
 type DocState = {
@@ -119,10 +64,8 @@ class Doc extends Component<DocProps, DocState> {
                         <View style={styles.breakable2} wrap={false} />
                         <View style={styles.breakable3} wrap={false} />
                     </View> */}
-                    <ProgressBarPDF allCards={this.props.allCards} finishedCards={this.props.finishedCards}/>
                 </Page>
             </Document>
-
         )
     };
 }
